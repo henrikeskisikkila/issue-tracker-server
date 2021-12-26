1a34a4fb491e83655aa7082542a0371b7017b3f3
henrikeskisikkila <henri@keskisikkila.fi>
2021-12-26 19:24:09 +0200

    Configuration update

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import issue from './routes/issue';
+
+const keys = require('./config/keys');
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
-});
+app.use('/issue', issue);
 
-app.listen(3000, () => {
-  return console.log('Server is listening at port 3000');
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+  return console.log(`Server is listening at port ${PORT}`);
 })
+
+export { app, mongoose, server };
